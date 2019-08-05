7d364af4f6b76ba8efa2e6e1e9de57aba8b8727d
Giovanni Cavallari <jobacavallari@hotmail.com>
2019-08-05 18:04:40 -0300

    currency_id fixed

--- mercadopago/controllers/front/standard.php
+++ mercadopago/controllers/front/standard.php
@@ -155,7 +155,7 @@ class MercadoPagoStandardModuleFrontController extends ModuleFrontController
                 'picture_url' => ('https://' ? 'https://' : 'http://').$imagePath,
                 'category_id' => $mercadopagoSettings['category_id'],
                 'quantity' => $product['quantity'],
-                "currency_id" => $this->context->currency->iso_code,
+                "currency_id" => $cart->id_currency,
                 'unit_price' => $product['price_wt'],
             );
             
@@ -170,7 +170,7 @@ class MercadoPagoStandardModuleFrontController extends ModuleFrontController
                 'description' => 'Wrapping service used by store',
                 'category_id' => $mercadopagoSettings['category_id'],
                 'quantity' => 1,
-                'currency_id' => $this->context->currency->iso_code,
+                'currency_id' => $cart->id_currency,
                 'unit_price' => $wrapping_cost,
             );
             $items[] = $item;
