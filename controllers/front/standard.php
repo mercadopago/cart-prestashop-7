683a22564d797db6bb2526af2218c60cd1395669
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-03-11 15:13:08 -0300

    Order creation change from controllers/front/standard to controllers/front/standardvalidation

--- controllers/front/standard.php
+++ controllers/front/standard.php
@@ -1,34 +1,33 @@
 <?php
 /**
- * 2007-2020 PrestaShop
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- *
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2007-2020 PrestaShop SA
- *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- *  International Registered Trademark & Property of PrestaShop SA
- *
- * Don't forget to prefix your containers with your own identifier
- * to avoid any conflicts with others containers.
- */
+* 2007-2020 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2020 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*
+* Don't forget to prefix your containers with your own identifier
+* to avoid any conflicts with others containers.
+*/
 
 require_once MP_ROOT_URL . '/includes/module/preference/StandardPreference.php';
-require_once MP_ROOT_URL . '/includes/module/notification/IpnNotification.php';
 
 class MercadoPagoStandardModuleFrontController extends ModuleFrontController
 {
@@ -50,7 +49,7 @@ class MercadoPagoStandardModuleFrontController extends ModuleFrontController
 
         //modal checkout
         if ($preference->settings['MERCADOPAGO_STANDARD_MODAL'] != "") {
-            return $this->standardModalCheckout($cart, $preference);
+            return $this->standardModalCheckout($preference);
         }
 
         //redirect checkout
@@ -70,7 +69,7 @@ class MercadoPagoStandardModuleFrontController extends ModuleFrontController
         if (is_array($createPreference) && array_key_exists('init_point', $createPreference)) {
             $preference->saveCreatePreferenceData($cart, $createPreference['notification_url']);
             MPLog::generate('Cart id ' . $cart->id . ' - Preference created successfully');
-            $this->createOrder($cart, $createPreference['init_point']);
+            return Tools::redirectLink($createPreference['init_point']);
         }
 
         return $preference->redirectError();
@@ -82,29 +81,14 @@ class MercadoPagoStandardModuleFrontController extends ModuleFrontController
      * @param mixed $cart
      * @return void
      */
-    public function standardModalCheckout($cart, $preference)
+    public function standardModalCheckout($preference)
     {
         $back_url = Tools::getValue('back_url');
 
         if (isset($back_url)) {
-            $this->createOrder($cart, $back_url);
+            return Tools::redirectLink($back_url);
         }
 
         return $preference->redirectError();
-    }
-
-    /**
-     * Create order without notification
-     *
-     * @param mixed $cart
-     * @param string $url
-     * @return void
-     */
-    public function createOrder($cart, $url) {
-        $customer_secure_key = $cart->secure_key;
-        $notification = new IpnNotification(null, $customer_secure_key);
-        $notification = $notification->createStandardOrder($cart);
-
-        return Tools::redirectLink($url);
     }
 }
--- controllers/front/standardvalidation.php
+++ controllers/front/standardvalidation.php
@@ -1,95 +1,83 @@
 <?php
 /**
-* 2007-2020 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2020 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*
-* Don't forget to prefix your containers with your own identifier
-* to avoid any conflicts with others containers.
-*/
+ * 2007-2020 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2020 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ *
+ * Don't forget to prefix your containers with your own identifier
+ * to avoid any conflicts with others containers.
+ */
+
+require_once MP_ROOT_URL . '/includes/module/notification/IpnNotification.php';
 
 class MercadoPagoStandardValidationModuleFrontController extends ModuleFrontController
 {
     public function initContent()
     {
-        $mercadopago = MPApi::getInstance();
+        $typeReturn = Tools::getValue('typeReturn');
         $payments_id = Tools::getValue('collection_id');
 
-        //failure payments
-        if (Tools::getValue('typeReturn') == 'failure') {
-            $this->redirectError();
-        }
-
-        //aproved and pending payments
-        if (isset($payments_id) && $payments_id != 'null') {
-            //treatment for payments data
-            $collection_ids = explode(',', $payments_id);
-            $payments_id = array();
-            $payments_type = array();
-            $payments_method = array();
-            $payments_status = array();
-            $payments_amount = array();
-
-            foreach ($collection_ids as $collection_id) {
-                $payment_info = $mercadopago->getPaymentStandard($collection_id);
-                if ($payment_info == false) {
-                    continue;
-                }
-
-                $payments_id[] = $payment_info['id'];
-                $payments_type[] = $payment_info['payment_type_id'];
-                $payments_method[] = $payment_info['payment_method_id'];
-                $payments_status[] = $payment_info['status'];
-                $payments_amount[] = $payment_info['transaction_amount'];
-                $cart_id = $payment_info['external_reference'];
-            }
-
-            //order confirmation redirect
+        if (isset($payments_id) && $payments_id != 'null' && $typeReturn != 'failure') {
+            $cart_id = Tools::getValue('external_reference');
             $cart = new Cart($cart_id);
             $total = $cart->getOrderTotal();
-            $order_id = Order::getOrderByCartId($cart->id);
-            $order = new Order($order_id);
 
             $uri = __PS_BASE_URI__ . 'index.php?controller=order-confirmation';
-            $uri .= '&id_cart=' . $order->id_cart;
-            $uri .= '&key=' . $order->secure_key;
-            $uri .= '&id_order=' . $order->id;
+            $uri .= '&id_cart=' . $cart_id;
             $uri .= '&id_module=' . $this->module->id;
-            $uri .= '&typeReturn=' . Tools::getValue('typeReturn');
+            $uri .= '&typeReturn=' . $typeReturn;
             $uri .= '&payment_id=' . implode(',', $payments_id);
-            $uri .= '&payment_type=' . implode(',', $payments_type);
-            $uri .= '&payment_method=' . implode(',', $payments_method);
-            $uri .= '&payment_status=' . implode(',', $payments_status);
-            $uri .= '&payment_amount=' . implode(',', $payments_amount);
-            $uri .= '&amount=' . $total;
+            $uri .= '&total=' . $total;
 
-            //redirect to order confirmation page
-            Tools::redirect($uri);
+            $this->createOrder($cart, $uri);
         }
+
+        $this->redirectError();
     }
 
-    protected function redirectError()
+    /**
+     * Create order without notification
+     *
+     * @param mixed $cart
+     * @param string $url
+     * @return void
+     */
+    public function createOrder($cart, $url)
     {
-        MPLog::generate('The callback failed', 'error');
-        Tools::redirect('index.php?controller=order&step=1&step=3&typeReturn=failure');
+        $customer_secure_key = $cart->secure_key;
+        $notification = new IpnNotification(null, $customer_secure_key);
+        $notification = $notification->createStandardOrder($cart);
+
+        return Tools::redirectLink($url);
+    }
+
+    /**
+     * Redirect if any errors occurs
+     *
+     * @return void
+     */
+    public function redirectError()
+    {
+        MPLog::generate('The mercadopago checkout callback failed', 'error');
+        Tools::redirect('index.php?controller=order&step=3&typeReturn=failure');
     }
 }
