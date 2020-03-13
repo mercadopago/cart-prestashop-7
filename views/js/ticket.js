99a12f86274cf11611bd5bf635041fb82c0a7a2e
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-03-13 11:28:47 -0300

    Change cnpj validator

--- views/js/ticket.js
+++ views/js/ticket.js
@@ -239,63 +239,53 @@
      * @return {bool}
      */
     function validateCNPJ(strCNPJ) {
-        var numeros, digitos, soma, i, resultado, pos, tamanho, digitos_iguais;
+        strCNPJ = strCNPJ.replace(/[^\d]+/g, '');
 
-        strCNPJ = strCNPJ.replace('.', '');
-        strCNPJ = strCNPJ.replace('.', '');
-        strCNPJ = strCNPJ.replace('.', '');
-        strCNPJ = strCNPJ.replace('-', '');
-        strCNPJ = strCNPJ.replace('/', '');
-        digitos_iguais = 1;
+        if (strCNPJ == '') return false;
 
-        if (strCNPJ.length < 14 && strCNPJ.length < 15) {
+        if (strCNPJ.length != 14)
             return false;
-        }
-        for (i = 0; i < strCNPJ.length - 1; i++) {
-            if (strCNPJ.charAt(i) !== strCNPJ.charAt(i + 1)) {
-                digitos_iguais = 0;
-                break;
-            }
-        }
-        if (!digitos_iguais) {
-            tamanho = strCNPJ.length - 2;
-            numeros = strCNPJ.substring(0, tamanho);
-            digitos = strCNPJ.substring(tamanho);
-            soma = 0;
-            pos = tamanho - 7;
 
-            for (i = tamanho; i >= 1; i--) {
-                soma += numeros.charAt(tamanho - i) * pos--;
-                if (pos < 2) {
-                    pos = 9;
-                }
-            }
-
-            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-            if (resultado !== digitos.charAt(0)) {
-                return false;
-            }
-
-            tamanho = tamanho + 1;
-            numeros = strCNPJ.substring(0, tamanho);
-            soma = 0;
-            pos = tamanho - 7;
-            for (i = tamanho; i >= 1; i--) {
-                soma += numeros.charAt(tamanho - i) * pos--;
-                if (pos < 2) {
-                    pos = 9;
-                }
-            }
-
-            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-            if (resultado !== digitos.charAt(1)) {
-                return false;
-            }
-
-            return true;
-        } else {
+        if (strCNPJ == '00000000000000' ||
+            strCNPJ == '11111111111111' ||
+            strCNPJ == '22222222222222' ||
+            strCNPJ == '33333333333333' ||
+            strCNPJ == '44444444444444' ||
+            strCNPJ == '55555555555555' ||
+            strCNPJ == '66666666666666' ||
+            strCNPJ == '77777777777777' ||
+            strCNPJ == '88888888888888' ||
+            strCNPJ == '99999999999999')
             return false;
+
+        tamanho = strCNPJ.length - 2;
+        numeros = strCNPJ.substring(0, tamanho);
+        digitos = strCNPJ.substring(tamanho);
+        soma = 0;
+        pos = tamanho - 7;
+        for (i = tamanho; i >= 1; i--) {
+            soma += numeros.charAt(tamanho - i) * pos--;
+            if (pos < 2)
+                pos = 9;
         }
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(0))
+            return false;
+
+        tamanho = tamanho + 1;
+        numeros = strCNPJ.substring(0, tamanho);
+        soma = 0;
+        pos = tamanho - 7;
+        for (i = tamanho; i >= 1; i--) {
+            soma += numeros.charAt(tamanho - i) * pos--;
+            if (pos < 2)
+                pos = 9;
+        }
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(1))
+            return false;
+
+        return true;
     }
 
 })();
