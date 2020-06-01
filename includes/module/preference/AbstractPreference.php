819b926a9f1ca99369f9b3eaab1d228511f337e2
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-06-01 12:59:48 -0300

    Fix statement descriptor

--- includes/module/preference/AbstractPreference.php
+++ includes/module/preference/AbstractPreference.php
@@ -1,31 +1,31 @@
 <?php
 /**
-* 2007-2020 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2020 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*
-* Don't forget to prefix your containers with your own identifier
-* to avoid any conflicts with others containers.
-*/
+ * 2007-2020 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2020 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ *
+ * Don't forget to prefix your containers with your own identifier
+ * to avoid any conflicts with others containers.
+ */
 
 class AbstractPreference
 {
@@ -84,7 +84,7 @@ class AbstractPreference
         $preference = array(
             'external_reference' => $cart->id,
             'notification_url' => $this->getNotificationUrl($cart),
-            'statement_descriptor' => $this->settings['MERCADOPAGO_INVOICE_NAME'],
+            'statement_descriptor' => $this->getStatementDescriptor(),
         );
 
         if (!$this->mercadopago->isTestUser()) {
@@ -201,6 +201,15 @@ class AbstractPreference
         }
 
         return $items;
+    }
+
+    public function getStatementDescriptor()
+    {
+        if ($this->settings['MERCADOPAGO_INVOICE_NAME'] == null) {
+            return '';
+        }
+
+        return $this->settings['MERCADOPAGO_INVOICE_NAME'];
     }
 
     /**
