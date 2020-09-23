8b4e35295a4f5abc836cbb1bcc32707560762a4f
Fernando Henriques <fernando.henriques@mercadolivre.com>
2020-09-23 18:24:08 -0300

    Minify all CSS assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
 
 const config = {
   scripts: [
@@ -9,13 +10,24 @@ const config = {
     './views/js/front.js',
     './views/js/ticket.js',
   ],
+  stylesheets: [
+    './views/css/front.css',
+    './views/css/back.css',
+  ],
 };
 
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
   return gulp.src(config.scripts)
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest('./views/js/'));
 });
 
-gulp.task('minify-assets', gulp.series('scripts'));
+gulp.task('stylesheets', () => {
+  return gulp.src(config.stylesheets)
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./views/css/'));
+});
+
+gulp.task('minify-assets', gulp.series('scripts', 'stylesheets'));
