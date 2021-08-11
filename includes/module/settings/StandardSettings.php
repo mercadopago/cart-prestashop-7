25cd06b48494e45465be596684feaae52024718e
cleitonaguiarandrade <cleiton.andrade@mercadolivre.com>
2021-08-11 14:52:45 -0300

    Used prestashop helper Tools::strltolower() instead of strtolower()

--- includes/module/settings/StandardSettings.php
+++ includes/module/settings/StandardSettings.php
@@ -309,7 +309,7 @@ class StandardSettings extends AbstractSettings
      */
     private function offlineExcludedPaymentMethodsCheck($payment_method){
     if($payment_method['type'] != 'account_money' &&
-        strtolower($payment_method['id']) != 'meliplace') { 
+        Tools::strtolower($payment_method['id']) != 'meliplace') { 
 
         return true;
         }
