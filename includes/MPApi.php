a0b165047ff29b4982d5acc18850ccff67fa738a
cleitonaguiarandrade <cleiton.andrade@mercadolivre.com>
2022-10-19 14:10:06 -0300

    Requested payment methods from bifrost instead MP

--- includes/MPApi.php
+++ includes/MPApi.php
@@ -111,8 +111,10 @@ class MPApi
      */
     public function getPaymentMethods()
     {
-        $access_token = $this->getAccessToken();
-        $response = MPRestCli::get('/v1/payment_methods', ["Authorization: Bearer " . $access_token]);
+        $public_key = $this->getPublicKey();
+        $response = MPRestCli::get('/v1/bifrost/payment-methods', ["Authorization: " . $public_key]);
+
+
 
         //in case of failures
         if ($response['status'] > 202) {
@@ -124,6 +126,11 @@ class MPApi
         $result = $response['response'];
         asort($result);
 
+
+        // echo('<pre>');
+        // print_r($result);
+        // die();
+
         $payments = array();
         foreach ($result as $value) {
             // remove on paypay release
@@ -133,9 +140,9 @@ class MPApi
 
             // PayCash
             if ($value['id'] == 'paycash') {
-                if (!isset($value['payment_places'])) {
+                if (isset($value['payment_places'])) {
                     $payments[] = array(
-                    'payment_places' => $this->getPaymentPlaces($value['id']),
+                    'payment_places' => $value['payment_places'],
                     'id' => Tools::strtoupper($value['id']),
                     'name' => $value['name'],
                     'type' => $value['payment_type_id'],
@@ -366,47 +373,47 @@ class MPApi
         }
     }
 
-  /**
-   * @param string|null $message
-   * @return string|null
-   */
-    public static function getPaymentPlaces($paymentId)
-    {
-        $payment_places = [
-            'paycash' => [
-                [
-                    "payment_option_id" => "7eleven",
-                    "name"              => "7 Eleven",
-                    "status"            => "active",
-                    "thumbnail"         => "https://http2.mlstatic.com/storage/logos-api-admin/417ddb90-34ab-11e9-b8b8-15cad73057aa-s.png"
-                ],
-                [
-                    "payment_option_id" => "circlek",
-                    "name"              => "Circle K",
-                    "status"            => "active",
-                    "thumbnail"         => "https://http2.mlstatic.com/storage/logos-api-admin/6f952c90-34ab-11e9-8357-f13e9b392369-s.png"
-                ],
-                [
-                    "payment_option_id" => "soriana",
-                    "name"              => "Soriana",
-                    "status"            => "active",
-                    "thumbnail"         => "https://http2.mlstatic.com/storage/logos-api-admin/dac0bf10-01eb-11ec-ad92-052532916206-s.png"
-                ],
-                [
-                    "payment_option_id" => "extra",
-                    "name"              => "Extra",
-                    "status"            => "active",
-                    "thumbnail"         => "https://http2.mlstatic.com/storage/logos-api-admin/9c8f26b0-34ab-11e9-b8b8-15cad73057aa-s.png"
-                ],
-                [
-                    "payment_option_id" => "calimax",
-                    "name"              => "Calimax",
-                    "status"            => "active",
-                    "thumbnail"         => "https://http2.mlstatic.com/storage/logos-api-admin/52efa730-01ec-11ec-ba6b-c5f27048193b-s.png"
-                ]
-            ],
-        ];
+//   /**
+//    * @param string|null $message
+//    * @return string|null
+//    */
+//     public static function getPaymentPlaces($paymentId)
+//     {
+//         $payment_places = [
+//             'paycash' => [
+//                 [
+//                     "payment_option_id" => "7eleven",
+//                     "name"              => "7 Eleven",
+//                     "status"            => "active",
+//                     "thumbnail"         => "https://http2.mlstatic.com/storage/logos-api-admin/417ddb90-34ab-11e9-b8b8-15cad73057aa-s.png"
+//                 ],
+//                 [
+//                     "payment_option_id" => "circlek",
+//                     "name"              => "Circle K",
+//                     "status"            => "active",
+//                     "thumbnail"         => "https://http2.mlstatic.com/storage/logos-api-admin/6f952c90-34ab-11e9-8357-f13e9b392369-s.png"
+//                 ],
+//                 [
+//                     "payment_option_id" => "soriana",
+//                     "name"              => "Soriana",
+//                     "status"            => "active",
+//                     "thumbnail"         => "https://http2.mlstatic.com/storage/logos-api-admin/dac0bf10-01eb-11ec-ad92-052532916206-s.png"
+//                 ],
+//                 [
+//                     "payment_option_id" => "extra",
+//                     "name"              => "Extra",
+//                     "status"            => "active",
+//                     "thumbnail"         => "https://http2.mlstatic.com/storage/logos-api-admin/9c8f26b0-34ab-11e9-b8b8-15cad73057aa-s.png"
+//                 ],
+//                 [
+//                     "payment_option_id" => "calimax",
+//                     "name"              => "Calimax",
+//                     "status"            => "active",
+//                     "thumbnail"         => "https://http2.mlstatic.com/storage/logos-api-admin/52efa730-01ec-11ec-ba6b-c5f27048193b-s.png"
+//                 ]
+//             ],
+//         ];
 
-        return $payment_places[$paymentId];
-    }
+//         return $payment_places[$paymentId];
+//     }
 }
