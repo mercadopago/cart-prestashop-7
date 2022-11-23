57cf272a6a9616de67226f34a29ddf71d3820ef2
katelucena-meli <kate.lucena@mercadolivre.com>
2022-11-23 13:27:25 -0300

    Adjustment to item decimals for payments in Checkout Pro

--- includes/module/preference/AbstractStandardPreference.php
+++ includes/module/preference/AbstractStandardPreference.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 2007-2022 PrestaShop
  *
@@ -101,6 +102,17 @@ abstract class AbstractStandardPreference extends AbstractPreference
     }
 
     /**
+     * Get calculate decimal
+     *
+     * @param $round, $unit_price
+     * @return mixed
+     */
+    public function getCalculateDecimalUnitPrice($round, $unit_price)
+    {
+        return $round ? Tools::ps_round($unit_price) : Tools::ps_round($unit_price, 2);
+    }
+
+    /**
      * Get customer data
      *
      * @param $cart
@@ -165,7 +177,7 @@ abstract class AbstractStandardPreference extends AbstractPreference
         }
 
         $paymentOptions = array(
-            'installments' => (integer) $this->settings['MERCADOPAGO_INSTALLMENTS'],
+            'installments' => (int) $this->settings['MERCADOPAGO_INSTALLMENTS'],
             'excluded_payment_types' => array(),
             'excluded_payment_methods' => $excludedPaymentMethods,
         );
